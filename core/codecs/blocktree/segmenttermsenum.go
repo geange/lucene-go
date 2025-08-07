a95e48d24ad93785e3ca452c4e58834496e88524
geange <geange@outlook.com>
2025-08-07 23:24:52 +0800

    chore: SegmentTermsEnum

--- core/codecs/blocktree/segmenttermsenum.go
+++ core/codecs/blocktree/segmenttermsenum.go
@@ -1,6 +1,7 @@
 package blocktree
 
 import (
+	"bytes"
 	"context"
 	"errors"
 
@@ -14,7 +15,52 @@ import (
 var _ index.TermsEnum = &SegmentTermsEnum{}
 
 func NewSegmentTermsEnum(fr *FieldReader) (*SegmentTermsEnum, error) {
-	panic("not implemented")
+	this := &SegmentTermsEnum{
+		scratchReader: store.NewByteArrayDataInput(nil),
+		arcs:          make([]*fst.Arc[[]byte], 1),
+		fr:            fr,
+		stack:         make([]*SegmentTermsEnumFrame, 0),
+	}
+
+	// Used to hold seek by TermState, or cached seek
+	staticFrame, err := NewSegmentTermsEnumFrame(this, -1)
+	if err != nil {
+		return nil, err
+	}
+	this.staticFrame = staticFrame
+
+	if fr.index == nil {
+		this.fstReader = nil
+	} else {
+		fstReader, err := fr.index.GetBytesReader()
+		if err != nil {
+			return nil, err
+		}
+		this.fstReader = fstReader
+	}
+
+	// Init w/ root block; don't use index since it may
+	// not (and need not) have been loaded
+	for i := range this.arcs {
+		this.arcs[i] = new(fst.Arc[[]byte])
+	}
+
+	this.currentFrame = staticFrame
+	//var arc *fst.Arc[[]byte]
+	if fr.index != nil {
+		_, err = fr.index.GetFirstArc(this.arcs[0])
+		if err != nil {
+			return nil, err
+		}
+		// Empty string prefix must have an output in the index!
+		//assert arc.isFinal();
+	} else {
+		//arc = nil
+	}
+	//currentFrame = pushFrame(arc, rootCode, 0);
+	//currentFrame.loadBlock();
+	this.validIndexPrefix = 0
+	return this, nil
 }
 
 type SegmentTermsEnum struct {
@@ -27,7 +73,7 @@ type SegmentTermsEnum struct {
 	staticFrame  *SegmentTermsEnumFrame
 	currentFrame *SegmentTermsEnumFrame
 	termExists   bool
-	fr           FieldReader
+	fr           *FieldReader
 
 	targetBeforeCurrentLength int
 
@@ -49,8 +95,115 @@ type SegmentTermsEnum struct {
 }
 
 func (s *SegmentTermsEnum) Next(ctx context.Context) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	var err error
+
+	if s.in == nil {
+
+		// Fresh TermsEnum; seek to first term:
+		var arc *fst.Arc[[]byte]
+
+		if s.fr.index != nil {
+			arc, err = s.fr.index.GetFirstArc(s.arcs[0])
+			// Empty string prefix must have an output in the index!
+			//assert arc.isFinal();
+		} else {
+			arc = nil
+		}
+
+		s.currentFrame, err = s.pushFrameSeekTo(ctx, arc, s.fr.rootCode, 0)
+		if err != nil {
+			return nil, err
+		}
+		if err := s.currentFrame.loadBlock(ctx); err != nil {
+			return nil, err
+		}
+	}
+
+	s.targetBeforeCurrentLength = s.currentFrame.ord
+
+	//assert !eof;
+	// if (DEBUG) {
+	//   System.out.println("\nBTTR.next seg=" + fr.parent.segment + " term=" + brToString(term) + " termExists?=" + termExists + " field=" + fr.fieldInfo.name + " termBlockOrd=" + currentFrame.state.termBlockOrd + " validIndexPrefix=" + validIndexPrefix);
+	//   printSeekState(System.out);
+	// }
+
+	if s.currentFrame == s.staticFrame {
+		// If seek was previously called and the term was
+		// cached, or seek(TermState) was called, usually
+		// caller is just going to pull a D/&PEnum or get
+		// docFreq, etc.  But, if they then call next(),
+		// this method catches up all internal state so next()
+		// works properly:
+		//if (DEBUG) System.out.println("  re-seek to pending term=" + term.utf8ToString() + " " + term);
+		if _, err := s.SeekExact(ctx, s.term); err != nil {
+			return nil, err
+		}
+		//assert result;
+	}
+
+	// Pop finished blocks
+	for s.currentFrame.nextEnt == s.currentFrame.entCount {
+		if !s.currentFrame.isLastInFloor {
+			// Advance to next floor block
+			if err := s.currentFrame.loadNextFloorBlock(ctx); err != nil {
+				return nil, err
+			}
+			break
+		} else {
+			//if (DEBUG) System.out.println("  pop frame");
+			if s.currentFrame.ord == 0 {
+				//if (DEBUG) System.out.println("  return null");
+				//assert setEOF();
+				s.term = s.term[:0]
+				s.validIndexPrefix = 0
+				if err := s.currentFrame.rewind(ctx); err != nil {
+					return nil, err
+				}
+				s.termExists = false
+				return nil, nil
+			}
+			lastFP := s.currentFrame.fpOrig
+			s.currentFrame = s.stack[s.currentFrame.ord-1]
+
+			if s.currentFrame.nextEnt == -1 || s.currentFrame.lastSubFP != lastFP {
+				// We popped into a frame that's not loaded
+				// yet or not scan'd to the right entry
+				if err := s.currentFrame.scanToFloorFrame(ctx, s.term); err != nil {
+					return nil, err
+				}
+				if err := s.currentFrame.loadBlock(ctx); err != nil {
+					return nil, err
+				}
+				if err := s.currentFrame.scanToSubBlock(ctx, lastFP); err != nil {
+					return nil, err
+				}
+			}
+
+			// Note that the seek state (last seek) has been
+			// invalidated beyond this depth
+			s.validIndexPrefix = min(s.validIndexPrefix, s.currentFrame.prefix)
+			//if (DEBUG) {
+			//System.out.println("  reset validIndexPrefix=" + validIndexPrefix);
+			//}
+		}
+	}
+
+	for {
+		if ok, _ := s.currentFrame.next(ctx); ok {
+			// Push to new block:
+			//if (DEBUG) System.out.println("  push frame");
+			s.currentFrame, err = s.pushFrame(ctx, nil, s.currentFrame.lastSubFP, len(s.term))
+			// This is a "next" frame -- even if it's
+			// floor'd we must pretend it isn't so we don't
+			// try to scan to the right floor frame:
+			if err := s.currentFrame.loadBlock(ctx); err != nil {
+				return nil, err
+			}
+		} else {
+			//if (DEBUG) System.out.println("  return term=" + brToString(term) + " currentFrame.ord=" + currentFrame.ord);
+			return s.term, nil
+		}
+	}
 }
 
 func ensureSize(a []byte, size int) []byte {
@@ -358,9 +511,320 @@ func (s *SegmentTermsEnum) SeekCeil(ctx context.Context, target []byte) (index.S
 	}
 }
 
+func (s *SegmentTermsEnum) SeekExact(ctx context.Context, target []byte) (bool, error) {
+	if s.fr.index == nil {
+		return false, errors.New("terms index was not loaded")
+	}
+
+	frSize, err := s.fr.Size()
+	if err != nil {
+		return false, err
+	}
+
+	getMin, err := s.fr.GetMin()
+	if err != nil {
+		return false, err
+	}
+	getMax, err := s.fr.GetMax()
+	if err != nil {
+		return false, err
+	}
+	if frSize > 0 && (bytes.Compare(target, getMin)) < 0 || bytes.Compare(target, getMax) > 0 {
+		return false, nil
+	}
+
+	s.term = ensureSize(s.term, len(target))
+
+	// if (DEBUG) {
+	//   System.out.println("\nBTTR.seekExact seg=" + fr.parent.segment + " target=" + fr.fieldInfo.name + ":" + brToString(target) + " current=" + brToString(term) + " (exists?=" + termExists + ") validIndexPrefix=" + validIndexPrefix);
+	//   printSeekState(System.out);
+	// }
+
+	var arc *fst.Arc[[]byte]
+	var targetUpto int
+	var output []byte
+
+	s.targetBeforeCurrentLength = s.currentFrame.ord
+
+	if s.currentFrame != s.staticFrame {
+
+		// We are already seek'd; find the common
+		// prefix of new seek term vs current term and
+		// re-use the corresponding seek state.  For
+		// example, if app first seeks to foobar, then
+		// seeks to foobaz, we can re-use the seek state
+		// for the first 5 bytes.
+
+		// if (DEBUG) {
+		//   System.out.println("  re-use current seek state validIndexPrefix=" + validIndexPrefix);
+		// }
+
+		arc = s.arcs[0]
+		//assert arc.isFinal();
+		output = arc.Output()
+		targetUpto = 0
+
+		lastFrame := s.stack[0]
+		//assert validIndexPrefix <= term.length();
+
+		targetLimit := min(len(target), s.validIndexPrefix)
+
+		cmp := 0
+
+		// TODO: reverse vLong byte order for better FST
+		// prefix output sharing
+
+		// First compare up to valid seek frames:
+		for targetUpto < targetLimit {
+			cmp = int(s.term[targetUpto] - target[targetUpto])
+			// if (DEBUG) {
+			//    System.out.println("    cycle targetUpto=" + targetUpto + " (vs limit=" + targetLimit + ") cmp=" + cmp + " (targetLabel=" + (char) (target.bytes[target.offset + targetUpto]) + " vs termLabel=" + (char) (term.bytes[targetUpto]) + ")"   + " arc.output=" + arc.output + " output=" + output);
+			// }
+			if cmp != 0 {
+				break
+			}
+			arc = s.arcs[1+targetUpto]
+			//assert arc.label() == (target.bytes[target.offset + targetUpto] & 0xFF): "arc.label=" + (char) arc.label() + " targetLabel=" + (char) (target.bytes[target.offset + targetUpto] & 0xFF);
+			if len(arc.Output()) != 0 {
+				output, err = FST_OUTPUTS.Add(output, arc.Output())
+				if err != nil {
+					return false, err
+				}
+			}
+			if arc.IsFinal() {
+				lastFrame = s.stack[1+lastFrame.ord]
+			}
+			targetUpto++
+		}
+
+		if cmp == 0 {
+			targetUptoMid := targetUpto
+
+			// Second compare the rest of the term, but
+			// don't save arc/output/frame; we only do this
+			// to find out if the target term is before,
+			// equal or after the current term
+			targetLimit2 := min(len(target), len(s.term))
+			for targetUpto < targetLimit2 {
+				cmp = int(s.term[targetUpto] - target[targetUpto])
+				// if (DEBUG) {
+				//    System.out.println("    cycle2 targetUpto=" + targetUpto + " (vs limit=" + targetLimit + ") cmp=" + cmp + " (targetLabel=" + (char) (target.bytes[target.offset + targetUpto]) + " vs termLabel=" + (char) (term.bytes[targetUpto]) + ")");
+				// }
+				if cmp != 0 {
+					break
+				}
+				targetUpto++
+			}
+
+			if cmp == 0 {
+				cmp = len(s.term) - len(target)
+			}
+			targetUpto = targetUptoMid
+		}
+
+		if cmp < 0 {
+			// Common case: target term is after current
+			// term, ie, app is seeking multiple terms
+			// in sorted order
+			// if (DEBUG) {
+			//   System.out.println("  target is after current (shares prefixLen=" + targetUpto + "); frame.ord=" + lastFrame.ord);
+			// }
+			s.currentFrame = lastFrame
+
+		} else if cmp > 0 {
+			// Uncommon case: target term
+			// is before current term; this means we can
+			// keep the currentFrame but we must rewind it
+			// (so we scan from the start)
+			s.targetBeforeCurrentLength = lastFrame.ord
+			// if (DEBUG) {
+			//   System.out.println("  target is before current (shares prefixLen=" + targetUpto + "); rewind frame ord=" + lastFrame.ord);
+			// }
+			s.currentFrame = lastFrame
+			if err := s.currentFrame.rewind(ctx); err != nil {
+				return false, err
+			}
+		} else {
+			// Target is exactly the same as current term
+			//assert term.length() == target.length;
+			if s.termExists {
+				// if (DEBUG) {
+				//   System.out.println("  target is same as current; return true");
+				// }
+				return true, nil
+			} else {
+				// if (DEBUG) {
+				//   System.out.println("  target is same as current but term doesn't exist");
+				// }
+			}
+			//validIndexPrefix = currentFrame.depth;
+			//term.length = target.length;
+			//return termExists;
+		}
+
+	} else {
+
+		s.targetBeforeCurrentLength = -1
+		arc, err = s.fr.index.GetFirstArc(s.arcs[0])
+		if err != nil {
+			return false, err
+		}
+
+		// Empty string prefix must have an output (block) in the index!
+		//assert arc.isFinal();
+		//assert arc.output() != null;
+
+		// if (DEBUG) {
+		//   System.out.println("    no seek state; push root frame");
+		// }
+
+		output = arc.Output()
+
+		s.currentFrame = s.staticFrame
+
+		//term.length = 0;
+		targetUpto = 0
+		frameData, err := FST_OUTPUTS.Add(output, arc.NextFinalOutput())
+		if err != nil {
+			return false, err
+		}
+		s.currentFrame, err = s.pushFrameSeekTo(ctx, arc, frameData, 0)
+		if err != nil {
+			return false, err
+		}
+	}
+
+	// if (DEBUG) {
+	//   System.out.println("  start index loop targetUpto=" + targetUpto + " output=" + output + " currentFrame.ord=" + currentFrame.ord + " targetBeforeCurrentLength=" + targetBeforeCurrentLength);
+	// }
+
+	// We are done sharing the common prefix with the incoming target and where we are currently seek'd; now continue walking the index:
+	for targetUpto < len(target) {
+
+		targetLabel := int(target[targetUpto])
+
+		nextArc, ok, err := s.fr.index.FindTargetArc(ctx, targetLabel, s.fstReader, arc, s.getArc(1+targetUpto))
+		if err != nil {
+			return false, err
+		}
+
+		if !ok {
+
+			// Index is exhausted
+			// if (DEBUG) {
+			//   System.out.println("    index: index exhausted label=" + ((char) targetLabel) + " " + toHex(targetLabel));
+			// }
+
+			s.validIndexPrefix = s.currentFrame.prefix
+			//validIndexPrefix = targetUpto;
+
+			if err := s.currentFrame.scanToFloorFrame(ctx, target); err != nil {
+				return false, err
+			}
+
+			if !s.currentFrame.hasTerms {
+				s.termExists = false
+				s.term[targetUpto] = byte(targetLabel)
+				s.term = s.term[:1+targetUpto]
+				// if (DEBUG) {
+				//   System.out.println("  FAST NOT_FOUND term=" + brToString(term));
+				// }
+				return false, nil
+			}
+
+			if err := s.currentFrame.loadBlock(ctx); err != nil {
+				return false, err
+			}
+
+			result, err := s.currentFrame.scanToTerm(ctx, target, true)
+			if err != nil {
+				return false, err
+			}
+			if result == index.SEEK_STATUS_FOUND {
+				// if (DEBUG) {
+				//   System.out.println("  return FOUND term=" + term.utf8ToString() + " " + term);
+				// }
+				return true, nil
+			} else {
+				// if (DEBUG) {
+				//   System.out.println("  got " + result + "; return NOT_FOUND term=" + brToString(term));
+				// }
+				return false, nil
+			}
+		} else {
+			// Follow this arc
+			arc = nextArc
+			s.term[targetUpto] = byte(targetLabel)
+			// Aggregate output as we go:
+			//assert arc.output() != null;
+			if FST_OUTPUTS.IsNoOutput(arc.Output()) {
+				output, err = FST_OUTPUTS.Add(output, arc.Output())
+				if err != nil {
+					return false, err
+				}
+			}
+
+			// if (DEBUG) {
+			//   System.out.println("    index: follow label=" + toHex(target.bytes[target.offset + targetUpto]&0xff) + " arc.output=" + arc.output + " arc.nfo=" + arc.nextFinalOutput);
+			// }
+			targetUpto++
+
+			if arc.IsFinal() {
+				//if (DEBUG) System.out.println("    arc is final!");
+				frameData, err := FST_OUTPUTS.Add(output, arc.NextFinalOutput())
+				if err != nil {
+					return false, err
+				}
+				s.currentFrame, err = s.pushFrameSeekTo(ctx, arc, frameData, targetUpto)
+				if err != nil {
+					return false, err
+				}
+				//if (DEBUG) System.out.println("    curFrame.ord=" + currentFrame.ord + " hasTerms=" + currentFrame.hasTerms);
+			}
+		}
+	}
+
+	//validIndexPrefix = targetUpto;
+	s.validIndexPrefix = s.currentFrame.prefix
+
+	if err := s.currentFrame.scanToFloorFrame(ctx, target); err != nil {
+		return false, err
+	}
+
+	// Target term is entirely contained in the index:
+	if !s.currentFrame.hasTerms {
+		s.termExists = false
+		s.term = s.term[:targetUpto]
+		// if (DEBUG) {
+		//   System.out.println("  FAST NOT_FOUND term=" + brToString(term));
+		// }
+		return false, nil
+	}
+
+	if err := s.currentFrame.loadBlock(ctx); err != nil {
+		return false, err
+	}
+
+	result, err := s.currentFrame.scanToTerm(ctx, target, true)
+	if err != nil {
+		return false, err
+	}
+	if result == index.SEEK_STATUS_FOUND {
+		// if (DEBUG) {
+		//   System.out.println("  return FOUND term=" + term.utf8ToString() + " " + term);
+		// }
+		return true, nil
+	} else {
+		// if (DEBUG) {
+		//   System.out.println("  got result " + result + "; return NOT_FOUND term=" + term.utf8ToString());
+		// }
+
+		return false, nil
+	}
+}
+
 func (s *SegmentTermsEnum) SeekExactByOrd(ctx context.Context, ord int64) error {
-	//TODO implement me
-	panic("implement me")
+	return errors.New("unsupported operation")
 }
 
 func (s *SegmentTermsEnum) Term() ([]byte, error) {
@@ -410,7 +874,29 @@ func (s *SegmentTermsEnum) initIndexInput() {
 }
 
 func (s *SegmentTermsEnum) pushFrameSeekTo(ctx context.Context, arc *fst.Arc[[]byte], frameData []byte, length int) (*SegmentTermsEnumFrame, error) {
-	panic("implement me")
+	s.scratchReader.Reset(frameData)
+	code, err := s.scratchReader.ReadUvarint(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fpSeek := int64(code >> OUTPUT_FLAGS_NUM_BITS)
+	f, err := s.getFrame(1 + s.currentFrame.ord)
+	if err != nil {
+		return nil, err
+	}
+	f.hasTerms = (code & OUTPUT_FLAG_HAS_TERMS) != 0
+	f.hasTermsOrig = f.hasTerms
+	f.isFloor = (code & OUTPUT_FLAG_IS_FLOOR) != 0
+	if f.isFloor {
+		if err := f.setFloorData(ctx, s.scratchReader, frameData); err != nil {
+			return nil, err
+		}
+	}
+	if _, err := s.pushFrame(ctx, arc, fpSeek, length); err != nil {
+		return nil, err
+	}
+	return f, nil
 }
 
 // Pushes next'd frame or seek'd frame; we later
--- core/util/fst/bytesequenceoutputs.go
+++ core/util/fst/bytesequenceoutputs.go
@@ -1,85 +1,151 @@
 package fst
 
 import (
+	"bytes"
 	"context"
+	"errors"
 
 	"github.com/geange/lucene-go/core/store"
 )
 
 var _ Outputs[[]byte] = &ByteSequenceOutputs{}
 
-type ByteSequenceOutputs struct{}
+type ByteSequenceOutputs struct {
+	noOutput []byte
+}
 
 func NewByteSequenceOutputs() *ByteSequenceOutputs {
-	return &ByteSequenceOutputs{}
+	return &ByteSequenceOutputs{
+		noOutput: make([]byte, 0),
+	}
 }
 
 func (b *ByteSequenceOutputs) Common(output1, output2 []byte) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+
+	pos := 0
+	pos2 := 0
+
+	stopAt := min(len(output1), len(output2))
+	for i := 0; i < stopAt; i++ {
+		if output1[i] != output2[i] {
+			break
+		}
+		pos++
+	}
+
+	if pos == 0 {
+		// no common prefix
+		return b.noOutput, nil
+	}
+
+	if pos == len(output1) {
+		// output1 is a prefix of output2
+		return output1, nil
+	}
+
+	if pos2 == len(output2) {
+		// output2 is a prefix of output1
+		return output2, nil
+	}
+
+	return output1[:pos], nil
 }
 
-func (b *ByteSequenceOutputs) Subtract(output1, inc []byte) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+func (b *ByteSequenceOutputs) Subtract(output, inc []byte) ([]byte, error) {
+	if (len(inc)) == 0 {
+		// no prefix removed
+		return output, nil
+	}
+
+	if !bytes.HasPrefix(output, inc) {
+		return nil, errors.New("output does not start with inc prefix")
+	}
+	if len(inc) == len(output) {
+		// entire output removed
+		return b.noOutput, nil
+	}
+	return bytes.TrimPrefix(output, inc), nil
 }
 
 func (b *ByteSequenceOutputs) Add(prefix, output []byte) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	if len(prefix) == 0 {
+		return output, nil
+	}
+
+	if len(output) == 0 {
+		return prefix, nil
+	}
+
+	return bytes.Join([][]byte{prefix, output}, []byte{}), nil
 }
 
 func (b *ByteSequenceOutputs) Merge(first, second []byte) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	return nil, errors.New("merge is not supported")
 }
 
 func (b *ByteSequenceOutputs) Write(ctx context.Context, output []byte, out store.DataOutput) error {
-	//TODO implement me
-	panic("implement me")
+	if err := out.WriteUvarint(ctx, uint64(len(output))); err != nil {
+		return err
+	}
+	if _, err := out.Write(output); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (b *ByteSequenceOutputs) WriteFinalOutput(ctx context.Context, output []byte, out store.DataOutput) error {
-	//TODO implement me
-	panic("implement me")
+	return b.Write(ctx, output, out)
 }
 
 func (b *ByteSequenceOutputs) Read(ctx context.Context, in store.DataInput) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	num, err := in.ReadUvarint(ctx)
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, num)
+	if _, err := in.Read(buf); err != nil {
+		return nil, err
+	}
+	return buf, nil
 }
 
 func (b *ByteSequenceOutputs) SkipOutput(ctx context.Context, in store.DataInput) error {
-	//TODO implement me
-	panic("implement me")
+	_, err := in.ReadUvarint(ctx)
+	return err
 }
 
 func (b *ByteSequenceOutputs) ReadFinalOutput(ctx context.Context, in store.DataInput) ([]byte, error) {
-	//TODO implement me
-	panic("implement me")
+	return b.Read(ctx, in)
 }
 
 func (b *ByteSequenceOutputs) SkipFinalOutput(ctx context.Context, in store.DataInput) error {
-	//TODO implement me
-	panic("implement me")
+	return b.SkipOutput(ctx, in)
 }
 
 func (b *ByteSequenceOutputs) IsNoOutput(v []byte) bool {
-	//TODO implement me
-	panic("implement me")
+	return len(v) == 0
 }
 
 func (b *ByteSequenceOutputs) GetNoOutput() []byte {
-	//TODO implement me
-	panic("implement me")
+	return b.noOutput
 }
 
 func (b *ByteSequenceOutputs) Equal(b1, b2 []byte) bool {
-	//TODO implement me
-	panic("implement me")
+	return bytes.Equal(b1, b2)
 }
 
 func (b *ByteSequenceOutputs) Hash(v []byte) int64 {
-	//TODO implement me
-	panic("implement me")
+	return bytesHashCode(v)
+}
+
+func bytesHashCode(bytes []byte) int64 {
+	if bytes == nil {
+		return 0
+	}
+
+	result := int64(1)
+	for _, b := range bytes {
+		result = 31*result + int64(int8(b))
+	}
+	return result
 }
