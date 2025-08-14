ac27367d5d66f9cf938f1b4fc2b2871b63bafa53
geange <geange@outlook.com>
2025-08-15 00:16:15 +0800

    chore: lucene80完善NormsProducer

--- core/codecs/lucene80/normsproducer.go
+++ core/codecs/lucene80/normsproducer.go
@@ -5,11 +5,21 @@ import (
 
 	"github.com/geange/lucene-go/core/document"
 	"github.com/geange/lucene-go/core/interface/index"
+	"github.com/geange/lucene-go/core/store"
 )
 
 var _ index.NormsProducer = &NormsProducer{}
 
 type NormsProducer struct {
+	// metadata maps (just file pointers and minimal stuff)
+	//private final Map<Integer,NormsEntry> norms = new HashMap<>();
+	norms          map[int]*NormsEntry
+	maxDoc         int
+	data           store.IndexInput
+	merging        bool
+	disiInputs     map[int]store.IndexInput
+	disiJumpTables map[int]*store.RandomAccessInput
+	dataInputs     map[int]*store.RandomAccessInput
 }
 
 func NewNormsProducer(ctx context.Context, state *index.SegmentReadState,
@@ -35,4 +45,14 @@ func (n *NormsProducer) CheckIntegrity() error {
 func (n *NormsProducer) GetMergeInstance() index.NormsProducer {
 	//TODO implement me
 	panic("implement me")
+}
+
+type NormsEntry struct {
+	denseRankPower      byte
+	bytesPerNorm        byte
+	docsWithFieldOffset int64
+	docsWithFieldLength int64
+	jumpTableEntryCount int
+	numDocsWithField    int
+	normsOffset         int64
 }
