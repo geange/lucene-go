760bb383086b446ca20700ce30199b856486b92f
geange <geange@outlook.com>
2025-08-20 00:15:22 +0800

    chore: IndexedDISI structure

--- core/codecs/lucene80/indexeddisi.go
+++ core/codecs/lucene80/indexeddisi.go
@@ -3,12 +3,56 @@ package lucene80
 import (
 	"context"
 
+	"github.com/geange/lucene-go/core/store"
 	"github.com/geange/lucene-go/core/types"
+)
+
+// jump-table time/space trade-offs to consider:
+// The block offsets and the block indexes could be stored in more compressed form with
+// two PackedInts or two MonotonicDirectReaders.
+// The DENSE ranks (default 128 shorts = 256 bytes) could likewise be compressed. But as there is
+// at least 4096 set bits in DENSE blocks, there will be at least one rank with 2^12 bits, so it
+// is doubtful if there is much to gain here.
+
+const (
+	BLOCK_SIZE               = 65536
+	DENSE_BLOCK_LONGS        = BLOCK_SIZE / 64
+	DEFAULT_DENSE_RANK_POWER = 9
+	MAX_ARRAY_LENGTH         = (1 << 12) - 1
 )
 
 var _ types.DocIdSetIterator = &IndexedDISI{}
 
 type IndexedDISI struct {
+	slice               store.IndexInput
+	jumpTableEntryCount int
+	denseRankPower      byte
+	jumpTable           store.RandomAccessInput // Skip blocks of 64K bits
+	denseRankTable      []byte
+	cost                int64
+
+	block             int
+	blockEnd          int64
+	denseBitmapOffset int64
+	nextBlockIndex    int
+	method            Method
+
+	doc   int
+	index int
+
+	// SPARSE variables
+	exists bool
+
+	// DENSE variables
+	word      int64
+	wordIndex int
+	// number of one bits encountered so far, including those of `word`
+	numberOfOnes int
+	// Used with rank for jumps inside of DENSE as they are absolute instead of relative
+	denseOrigoIndex int
+
+	// ALL variables
+	gap int
 }
 
 func (i *IndexedDISI) DocID() int {
@@ -39,3 +83,5 @@ func (i *IndexedDISI) Cost() int64 {
 func (i *IndexedDISI) AdvanceExact(ctx context.Context, target int) (bool, error) {
 	panic("implement me")
 }
+
+type Method struct{}
