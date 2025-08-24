8273f6e40ce7388f6aa5d7c78def224ee1cadc21
geange <geange@outlook.com>
2025-08-24 22:26:27 +0800

    chore: NormsConsumer.writeValues

--- core/codecs/lucene80/normsconsumer.go
+++ core/codecs/lucene80/normsconsumer.go
@@ -2,9 +2,12 @@ package lucene80
 
 import (
 	"context"
+	"errors"
+	"io"
 
 	"github.com/geange/lucene-go/core/document"
 	"github.com/geange/lucene-go/core/interface/index"
+	"github.com/geange/lucene-go/core/store"
 )
 
 var _ index.NormsConsumer = &NormsConsumer{}
@@ -35,4 +38,41 @@ func (n *NormsConsumer) Merge(ctx context.Context, mergeState *index.MergeState)
 func (n *NormsConsumer) MergeNormsField(ctx context.Context, mergeFieldInfo *document.FieldInfo, mergeState *index.MergeState) error {
 	//TODO implement me
 	panic("implement me")
+}
+func (n *NormsConsumer) writeValues(ctx context.Context, values index.NumericDocValues,
+	numBytesPerValue int, out store.IndexOutput) error {
+
+	for {
+		if _, err := values.NextDoc(ctx); err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return err
+		}
+
+		value, err := values.LongValue()
+		if err != nil {
+			return err
+		}
+		switch numBytesPerValue {
+		case 1:
+			if err := out.WriteByte(byte(value)); err != nil {
+				return err
+			}
+		case 2:
+			if err := out.WriteUint16(ctx, uint16(value)); err != nil {
+				return err
+			}
+		case 4:
+			if err := out.WriteUint32(ctx, uint32(value)); err != nil {
+				return err
+			}
+		case 8:
+			if err := out.WriteUint64(ctx, uint64(value)); err != nil {
+				return err
+			}
+		default:
+			return errors.New("invalid numBytesPerValue")
+		}
+	}
 }
