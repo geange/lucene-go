f63d9a000b5bcd91a250a6020bcb195bf69c7f00
geange <geange@outlook.com>
2025-08-29 00:49:02 +0800

    chore: compress.LZ4

--- core/util/compress/lz4_lucene.go
+++ core/util/compress/lz4_lucene.go
@@ -11,19 +11,19 @@ const (
 	HASH_TABLE_SIZE_HC = 1 << HASH_LOG_HC
 )
 
-func (*LZ4) hash(i int32, hashBits int) int32 {
+func hash(i int32, hashBits int) int32 {
 	return int32(uint32(i*-1640531535) >> (32 - hashBits))
 }
 
-func (l *LZ4) hashHC(i int32) int32 {
-	return l.hash(i, HASH_LOG_HC)
+func hashHC(i int32) int32 {
+	return hash(i, HASH_LOG_HC)
 }
 
-func (l *LZ4) readInt(buf []byte, i int) int32 {
+func readInt(buf []byte, i int) int32 {
 	n := (uint32(buf[i]) << 24) | (uint32(buf[i+1]) << 16) | (uint32(buf[i+2]) << 8) | (uint32(buf[i+3]) & 0xFF)
 	return int32(n)
 }
 
-func (l *LZ4) commonBytes(b []byte, o1, o2, limit int) int {
+func commonBytes(b []byte, o1, o2, limit int) int {
 	return array.Mismatch(b[o1:o1+limit], b[o2:o2+limit])
 }
