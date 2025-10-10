e7ab4e5179e8efe07c0fb4401c6283d5e8fa4d10
geange <geange@outlook.com>
2025-10-11 00:49:43 +0800

    fix: lz4 compress

---
+++ core/util/compress/highcompression.go
@@ -0,0 +1,119 @@
+package compress
+
+import "github.com/geange/lucene-go/core/util/array"
+
+var _ HashTable = &HighCompressionHashTable{}
+
+type HighCompressionHashTable struct {
+	bytes      []byte
+	base       int
+	next       int
+	end        int
+	hashTable  []int
+	chainTable []int16
+	attempts   int
+}
+
+func NewHighCompressionHashTable() *HighCompressionHashTable {
+	table := &HighCompressionHashTable{
+		hashTable:  make([]int, HASH_TABLE_SIZE_HC),
+		chainTable: make([]int16, MAX_DISTANCE),
+	}
+
+	array.Fill(table.hashTable, -1)
+	v := uint16(0xFFFF)
+	array.Fill(table.chainTable, int16(v))
+	return table
+}
+
+func (h *HighCompressionHashTable) Reset(bs []byte) error {
+
+	m16 := uint16(0xFFFF)
+	if h.end-h.base < len(h.chainTable) {
+		// The last call to compress was done on less than 64kB, let's not reset
+		// the hashTable and only reset the relevant parts of the chainTable.
+		// This helps avoid slowing down calling compress() many times on short
+		// inputs.
+		startOffset := h.base & MASK
+		endOffset := 0
+		if h.end != 0 {
+			endOffset = (h.end-1)&MASK + 1
+		}
+
+		if startOffset < endOffset {
+			array.Fill(h.chainTable[startOffset:endOffset], int16(m16))
+		} else {
+			array.Fill(h.chainTable[:endOffset], int16(m16))
+			array.Fill(h.chainTable[startOffset:], int16(m16))
+		}
+	} else {
+		// The last call to compress was done on a large enough amount of data
+		// that it's fine to reset both tables
+		array.Fill(h.hashTable, -1)
+		array.Fill(h.chainTable, int16(m16))
+	}
+
+	h.bytes = bs
+	h.base = 0
+	h.next = 9
+	h.end = len(bs)
+	return nil
+}
+
+func (h *HighCompressionHashTable) InitDictionary(dictLen int) {
+	for i := 0; i < dictLen; i++ {
+		h.addHash(h.base + i)
+	}
+	h.next += dictLen
+}
+
+func (t *HighCompressionHashTable) Get(off int) (int, error) {
+	for ; t.next < off; t.next++ {
+		t.addHash(t.next)
+	}
+
+	v := readInt(t.bytes, off)
+	h := hashHC(v)
+
+	t.attempts = 0
+	ref := t.hashTable[h]
+	if ref >= off {
+		// remainder from a previous call to compress()
+		return -1, nil
+	}
+
+	min := max(t.base, off-MAX_DISTANCE+1)
+	for ref >= min && t.attempts < MAX_ATTEMPTS {
+		if readInt(t.bytes, ref) == v {
+			return ref, nil
+		}
+		ref -= int(uint16(t.chainTable[ref&MASK]) & 0xFFFF)
+		t.attempts++
+	}
+	return -1, nil
+}
+
+func (h *HighCompressionHashTable) Previous(off int) int {
+	v := readInt(h.bytes, off)
+	ref := off - int(uint16(h.chainTable[off&MASK])&0xFFFF)
+	for ref >= h.base && h.attempts < MAX_ATTEMPTS {
+		if readInt(h.bytes, ref) == v {
+			return ref
+		}
+		ref -= int(uint16(h.chainTable[ref&MASK]) & 0xFFFF)
+		h.attempts++
+	}
+	return -1
+}
+
+func (h *HighCompressionHashTable) addHash(off int) {
+	v := readInt(h.bytes, off)
+	code := hashHC(int32(v))
+
+	delta := off - h.hashTable[code]
+	if delta <= 0 || delta >= MAX_DISTANCE {
+		delta = MAX_DISTANCE - 1
+	}
+	h.chainTable[off&MASK] = int16(delta)
+	h.hashTable[code] = off
+}
--- core/util/compress/lz4_test.go
+++ core/util/compress/lz4_test.go
@@ -12,7 +12,9 @@ func TestCompress(t *testing.T) {
 
 	buf := store.NewBufferDataOutput()
 
-	Compress([]byte("{\"data\":[{\"name\":\"xxx\"},{\"name\":\"www\"}]}"), buf, ht)
+	t.Log("origin size", len([]byte(data)))
+
+	Compress([]byte(data), buf, ht)
 
 	t.Log(len(buf.Bytes()))
 
@@ -20,3 +22,149 @@ func TestCompress(t *testing.T) {
 	f.Write(buf.Bytes())
 	f.Close()
 }
+
+var data = `<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AutoImportSettings">
+    <option name="autoReloadType" value="ALL" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="6d1f9e8e-b1fa-4efd-93bd-11b313d7dc2f" name="更改" comment="chore: DocValuesConsumer.writeValuesMultipleBlocks" />
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="FileTemplateManagerImpl">
+    <option name="RECENT_TEMPLATES">
+      <list>
+        <option value="Go File" />
+      </list>
+    </option>
+  </component>
+  <component name="GOROOT" url="file:///usr/local/go" />
+  <component name="Git.Settings">
+    <option name="RECENT_BRANCH_BY_REPOSITORY">
+      <map>
+        <entry key="$PROJECT_DIR$" value="main" />
+      </map>
+    </option>
+    <option name="RECENT_GIT_ROOT_PATH" value="$PROJECT_DIR$" />
+    <option name="UPDATE_TYPE" value="REBASE" />
+  </component>
+  <component name="HighlightingSettingsPerFile">
+    <setting file="file:///usr/local/go/src/math/bits/bits.go" root0="SKIP_INSPECTION" />
+  </component>
+  <component name="ProjectColorInfo">{
+  &quot;associatedIndex&quot;: 3
+}</component>
+  <component name="ProjectId" id="30pYhxzBo6o1AcOf2quQwkNgHpS" />
+  <component name="ProjectViewState">
+    <option name="hideEmptyMiddlePackages" value="true" />
+    <option name="showLibraryContents" value="true" />
+  </component>
+  <component name="PropertiesComponent">{
+  &quot;keyToString&quot;: {
+    &quot;DefaultGoTemplateProperty&quot;: &quot;Go File&quot;,
+    &quot;Go 测试.github.com/geange/lucene-go/core/util/automaton 中的 TestFrozenIntSet_Equals/TC04_-_values_differ.executor&quot;: &quot;Debug&quot;,
+    &quot;ModuleVcsDetector.initialDetectionPerformed&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.GoLinterPluginOnboarding&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.GoLinterPluginStorageMigration&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.ShowReadmeOnStart&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.TerminalTabsStorage.copyFrom.TerminalArrangementManager&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.TerminalTabsStorage.copyFrom.TerminalArrangementManager.252&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.git.unshallow&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.go.formatter.settings.were.checked&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.go.migrated.go.modules.settings&quot;: &quot;true&quot;,
+    &quot;RunOnceActivity.go.modules.go.list.on.any.changes.was.set&quot;: &quot;true&quot;,
+    &quot;git-widget-placeholder&quot;: &quot;feature/lucene80&quot;,
+    &quot;go.import.settings.migrated&quot;: &quot;true&quot;,
+    &quot;last_opened_file_path&quot;: &quot;/Users/geange/workspace/lucene-go&quot;,
+    &quot;node.js.detected.package.eslint&quot;: &quot;true&quot;,
+    &quot;node.js.selected.package.eslint&quot;: &quot;(autodetect)&quot;,
+    &quot;nodejs_package_manager_path&quot;: &quot;npm&quot;,
+    &quot;settings.editor.selected.configurable&quot;: &quot;preferences.lookFeel&quot;
+  }
+}</component>
+  <component name="RecentsManager">
+    <key name="MoveFile.RECENT_KEYS">
+      <recent name="$PROJECT_DIR$/core/codecs/lucene80" />
+    </key>
+  </component>
+  <component name="RunManager">
+    <configuration name="github.com/geange/lucene-go/core/util/automaton 中的 TestFrozenIntSet_Equals/TC04_-_values_differ" type="GoTestRunConfiguration" factoryName="Go Test" temporary="true" nameIsGenerated="true">
+      <module name="lucene-go" />
+      <working_directory value="$PROJECT_DIR$/core/util/automaton" />
+      <root_directory value="$PROJECT_DIR$" />
+      <kind value="PACKAGE" />
+      <package value="github.com/geange/lucene-go/core/util/automaton" />
+      <directory value="$PROJECT_DIR$" />
+      <filePath value="$PROJECT_DIR$" />
+      <framework value="gotest" />
+      <pattern value="^\QTestFrozenIntSet_Equals\E$/^\QTC04_-_values_differ\E$" />
+      <method v="2" />
+    </configuration>
+    <recent_temporary>
+      <list>
+        <item itemvalue="Go 测试.github.com/geange/lucene-go/core/util/automaton 中的 TestFrozenIntSet_Equals/TC04_-_values_differ" />
+      </list>
+    </recent_temporary>
+  </component>
+  <component name="SharedIndexes">
+    <attachedChunks>
+      <set>
+        <option value="bundled-gosdk-f466f9b0953e-3d2cccfc42a2-org.jetbrains.plugins.go.sharedIndexes.bundled-GO-252.25557.175" />
+        <option value="bundled-js-predefined-d6986cc7102b-b598e85cdad2-JavaScript-GO-252.25557.175" />
+      </set>
+    </attachedChunks>
+  </component>
+  <component name="SpellCheckerSettings" RuntimeDictionaries="0" Folders="0" CustomDictionaries="0" DefaultDictionary="项目级" UseSingleDictionary="true" transferred="true" />
+  <component name="TaskManager">
+    <task active="true" id="Default" summary="默认任务">
+      <changelist id="6d1f9e8e-b1fa-4efd-93bd-11b313d7dc2f" name="更改" comment="" />
+      <created>1754326103972</created>
+      <option name="number" value="Default" />
+      <option name="presentableId" value="Default" />
+      <updated>1754326103972</updated>
+    </task>
+    <servers />
+  </component>
+  <component name="TypeScriptGeneratedFilesManager">
+    <option name="version" value="3" />
+  </component>
+  <component name="Vcs.Log.Tabs.Properties">
+    <option name="TAB_STATES">
+      <map>
+        <entry key="MAIN">
+          <value>
+            <State />
+          </value>
+        </entry>
+      </map>
+    </option>
+  </component>
+  <component name="VcsManagerConfiguration">
+    <MESSAGE value="fix: test fail" />
+    <MESSAGE value="chore: 20250806" />
+    <MESSAGE value="chore: 20250807" />
+    <MESSAGE value="chore: SegmentTermsEnum" />
+    <MESSAGE value="chore: TermsWriter" />
+    <MESSAGE value="chore: lucene80完善format" />
+    <MESSAGE value="chore: lucene80完善NormsProducer" />
+    <MESSAGE value="chore: NormsProducer init" />
+    <MESSAGE value="chore: IndexedDISI structure" />
+    <MESSAGE value="chore: IndexedDISI" />
+    <MESSAGE value="chore: NormsProducer" />
+    <MESSAGE value="chore: NormsConsumer.AddNormsField" />
+    <MESSAGE value="chore: NormsConsumer" />
+    <MESSAGE value="chore: NormsConsumer imports" />
+    <MESSAGE value="chore: update deps" />
+    <MESSAGE value="chore: DocValuesConsumer.CompressedBinaryBlockWriter" />
+    <MESSAGE value="chore: compress.LZ4" />
+    <MESSAGE value="chore: DocValuesConsumer.writeValuesMultipleBlocks" />
+    <option name="LAST_COMMIT_MESSAGE" value="chore: DocValuesConsumer.writeValuesMultipleBlocks" />
+  </component>
+  <component name="VgoProject">
+    <settings-migrated>true</settings-migrated>
+  </component>
+</project>`
